8977320f5a30b4e9cda016d64d4286981c113bb2
19047433 <19047433@DESKTOP-TDL3545>
2020-08-19 18:56:48 +0800

    NICOLE PROMOTION CANTEENAPP

--- C206_CaseStudy/src/CanteenApp.java
+++ C206_CaseStudy/src/CanteenApp.java
@@ -9,6 +9,7 @@ public class CanteenApp {
 		optionMenu();
 		int option = 0;
 		ArrayList<Order> OrderList = new ArrayList<Order>();
+		ArrayList<Promotion> promotionList = new ArrayList<Promotion>();
 		// End
 		
 		
@@ -59,9 +60,26 @@ public class CanteenApp {
 					
 					break;
 					
-				case 5: // Promotion
-					
-					
+				case 5: // Promotion -> NICOLE5
+					Helper.line(30, "-");
+					System.out.println("Promotion MENU");
+					Helper.line(30, "-");
+					System.out.println("1. View Promotion");
+					System.out.println("2. Add Promotion");
+					System.out.println("3. Delete Promotion");
+					int choice = Helper.readInt("Enter choice > ");
+					if(choice == 1) {
+						
+					}
+					else if(choice == 2){
+						
+					}
+					else if(choice == 3) {
+						
+					}
+					else {
+						System.out.println("Invalid Choice Input.");
+					}
 					break;
 					
 				case 6:
@@ -108,6 +126,19 @@ public class CanteenApp {
 	public static void viewAllOrder(ArrayList<Order> items) {	
 	}
 // End
+/*=============================================================================================================================*/
+
+/*=============================================================================================================================*/
+//Promotion ADD, VIEW DELETE BY NICOLE
+	public static void addPromotion(ArrayList<Promotion> promotionList) {	
+		
+	}
+	public static void deletePromotion(ArrayList<Promotion> promotionList) {	
+		
+	}
+	public static void viewAllPromotion(ArrayList<Promotion> PromotionList) {	
+		
+	}
 /*=============================================================================================================================*/
 
 }
