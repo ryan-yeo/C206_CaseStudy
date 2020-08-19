8c5e5e09feb193010fa88399ee5a7d169eeadfc7
19047433 <19047433@DESKTOP-TDL3545>
2020-08-19 18:53:10 +0800

    NICOLE Promotion.java

---
+++ C206_CaseStudy/src/Promotion.java
@@ -0,0 +1,33 @@
+
+public class Promotion {
+	private String promotionCode;
+	private String endDate;
+	private double discountAmount;
+	
+	public Promotion(String promotionCode, String endDate, double discountAmount) {
+		this.promotionCode = promotionCode;
+		this.endDate = endDate;
+		this.discountAmount = discountAmount;
+	}
+	public String getPromotionCode() {
+		return promotionCode;
+	}
+	public void setPromotionCode(String promotionCode) {
+		this.promotionCode = promotionCode;
+	}
+	public String getEndDate() {
+		return endDate;
+	}
+	public void setEndFate(String endDate) {
+		this.endDate = endDate;
+	}
+	public double getDiscountAmount() {
+		return discountAmount;
+	}
+	public void setDiscountAmount(double discountAmount) {
+		this.discountAmount = discountAmount;
+	}
+	public String toString() {
+		return null;
+	}
+}
