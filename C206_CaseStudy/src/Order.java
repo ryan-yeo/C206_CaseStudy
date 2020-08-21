3778a4c9ea53c7064a4de714a8c69ba695f83484
Bubbling <68381125+19045083-Vanessa-Wu@users.noreply.github.com>
2020-08-21 11:04:06 +0800

    Update Order.java

--- C206_CaseStudy/src/Order.java
+++ C206_CaseStudy/src/Order.java
@@ -51,7 +51,7 @@ public class Order {
 
 		String itemList = "";
 		for (int i=0; i < items.size(); i++) {
-			itemList += items.get(i).getItemName();
+			itemList += items.get(i).getName();
 			if (i != (items.size()-1)) {
 				itemList += ", ";
 			}
