ec4e4a27138b7edcb3ff15f6c054adeb7f38475b
19021683 <19021683@LAPTOP-70FG3802>
2020-08-21 10:13:49 +0800

    menu class creation

--- C206_CaseStudy/src/MenuItem.java
+++ C206_CaseStudy/src/MenuItem.java
@@ -1,4 +1,40 @@
 
 public class MenuItem {
 
+	private String category;
+	private String name;
+	private float price;
+	
+	public MenuItem(String category, String itemName, float price) {
+		this.category = category;
+		this.name = itemName;
+		this.price = price;
+	}
+	
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	public String getItemName() {
+		return name;
+	}
+	public void setItemName() {
+		this.name = name;
+	}
+	public float getPrice() {
+		return price;
+	}
+	public void setPrice() {
+		this.price = price;
+	}
+
+	public void setItemName(String itemName) {
+		this.name = itemName;
+	}
+
+	public void setPrice(float price) {
+		this.price = price;
+	}
 }
